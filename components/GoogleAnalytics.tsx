50738ac14b531da8b1c64386f11c96920bdfce25
ChaimaaAtraoui <chaima.atra@gmail.com>
2026-01-08 19:44:57 +0100

    Fix TypeScript errors in GoogleAnalytics

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -6,19 +6,21 @@ import { useEffect } from 'react'
 
 const GA_MEASUREMENT_ID = 'G-E45GDPSDW5'
 
+declare global {
+  interface Window {
+    gtag: (command: string, ...args: unknown[]) => void
+    dataLayer: unknown[]
+  }
+}
+
 export default function GoogleAnalytics() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
   useEffect(() => {
-    if (pathname) {
-      const url = pathname + (searchParams?.toString() ? `?${searchParams.toString()}` : '')
-      
-      if (typeof window !== 'undefined' && (window as any).gtag) {
-        (window as any).gtag('config', GA_MEASUREMENT_ID, {
-          page_path: url,
-        })
-      }
+    if (pathname && typeof window !== 'undefined' && window.gtag) {
+      const url = pathname + (searchParams?.toString() ? `?${searchParams}` : '')
+      window.gtag('config', GA_MEASUREMENT_ID, { page_path: url })
     }
   }, [pathname, searchParams])
 
@@ -28,20 +30,14 @@ export default function GoogleAnalytics() {
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
       />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
     </>
   )
 }
